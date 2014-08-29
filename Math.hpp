b7bf49debb56a0fa2da269fc90cb9225ab06c65b
James Gibson <jameshgibson@yahoo.co.uk>
2014-08-29 18:12:23 +0100

    removing debug output

--- Math.hpp
+++ Math.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 template<class T>
 bool epsilon_equals(const T &lhs, const T &rhs, double epsilon = 0.0000001)
 {
@@ -14,10 +12,8 @@ typename container::value_type average(const container &nums, typename container
     typename container::const_iterator it = nums.begin();
     for (; it < nums.end(); ++it)
     {
-	std::cout << *it << " ";
 	init += *it;
     }
-    std::cout << std::endl;
     return init / (typename container::value_type)nums.size();
 }
     
